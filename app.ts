9aebd74803cd65273171a1b01b41decea4dc58fb
MaYiLagann <lhwshy@naver.com>
2024-01-28 18:27:17 +0900

    Install typescript dev environments

---
+++ app.ts
@@ -0,0 +1,21 @@
+import express, { Express } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+// Import routes.
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+const app: Express = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export = app;
