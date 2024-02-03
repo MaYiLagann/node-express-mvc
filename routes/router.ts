a989afc4cf250b9e7046c34077a1d2f40d65e0a5
MaYiLagann <lhwshy@naver.com>
2024-02-03 22:04:45 +0900

    One-liner for router

--- routes/router.ts
+++ routes/router.ts
@@ -3,12 +3,7 @@ import { Request, Response, Router } from 'express';
 export const router = Router();
 
 /* GET home page. */
-router.get('/', (_req: Request, res: Response) => {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (_req: Request, res: Response) => res.render('index', { title: 'Express' }));
 
 /* GET users listing. */
-router.get('/users', (_req: Request, res: Response) => {
-  res.send('respond with a resource');
-});
-
+router.get('/users', (_req: Request, res: Response) => res.send('respond with a resource'));
