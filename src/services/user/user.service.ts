265b881ac2013cf792852cbe178e7c9d1fe193f8
MaYiLagann <lhwshy@naver.com>
2024-02-12 23:57:15 +0900

    Implement hashing user password with bcrypt

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,6 +1,11 @@
+import bcrypt from "bcrypt";
 import { injectable } from "inversify";
 import { PrismaClient, User } from "@prisma/client";
 import { IUserService } from "./user.service.interface";
+import { EnvironmentError } from "../../models/errors/environment-error";
+import { ArgumentError } from "../../models/errors/argument-error";
+
+const envPasswordSalt = 'USER_PASSWORD_SALT';
 
 @injectable()
 export class UserService implements IUserService {
@@ -11,13 +16,21 @@ export class UserService implements IUserService {
   }
 
   async signUp(user: User): Promise<User> {
+    if (!user.email.trim()) throw new ArgumentError(`User requires the field email`, 'user');
+    if (!user.password.trim()) throw new ArgumentError(`User requires the field password`, 'user');
+
+    const passwordSalt = process.env[envPasswordSalt];
+    if (!passwordSalt) throw new EnvironmentError(`Environment ${envPasswordSalt} is not set`, envPasswordSalt);
+
     const prisma = new PrismaClient();
 
     try {
+      const password = await bcrypt.hash(user.password, passwordSalt);
+
       const entity = await prisma.user.create({
         data: {
           email: user.email,
-          password: 'user.password', // Todo: Hash the password.
+          password: password,
           name: user.name,
         }
       });
