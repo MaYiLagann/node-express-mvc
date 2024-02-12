ac81f017cd1dbb1ff1166a6179217719b6ba335f
MaYiLagann <lhwshy@naver.com>
2024-02-12 20:37:40 +0900

    Update comment services

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,3 +1,7 @@
+/**
+ * List of service symbols that injectable via inversify.
+ * See also: https://github.com/inversify/InversifyJS/blob/master/wiki/symbols_as_id.md
+ */
 export const services = {
   UserService: Symbol.for('IUserService'),
 };
