b912375f2392e78b8834fabbd7203b4b61cc21f6
MaYiLagann <lhwshy@naver.com>
2024-02-13 00:55:56 +0900

    Create api router post user

--- routes/router.ts
+++ routes/router.ts
@@ -24,3 +24,21 @@ router.get('/users', async (_req: Request, res: Response) => {
   const controller = container.resolve(UserController);
   res.send(await controller.Get());
 });
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     description: Create a new user
+ *     tags: [Users]
+ *     produces:
+ *       - plain/text
+ *     responses:
+ *       200:
+ *         description:
+ */
+router.post('/user', async (req: Request, res: Response) => {
+  const controller = container.resolve(UserController);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-argument
+  res.send(await controller.Post(req.body.email, req.body.password, req.body.name));
+});
--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -6,7 +6,8 @@ import { IUserService } from "../../services/user/user.service.interface";
 export class UserController {
   public constructor(
     @inject(services.UserService) private userService: IUserService,
-  ) {}
+  ) {
+  }
 
   public async Get(): Promise<string> {
     const result = await this.userService.authorize('id', 'pw');
@@ -14,5 +15,14 @@ export class UserController {
       return 'respond with a resource';
     else
       throw new Error('failed to get user');
+  }
+
+  public async Post(email: string, password: string, name: string): Promise<void> {
+    await this.userService.signUp({
+      id: 0,
+      email: email,
+      password: password,
+      name: name,
+    });
   }
 }
